db0fdca9caeda0dfecbc752913a414f495aae7ca
unknown <l177829@unicamp.com>
2020-05-24 17:10:48 -0300

    3

--- tarefa05/capicuia.py
+++ tarefa05/capicuia.py
@@ -1,12 +1,14 @@
 n = int((input('digite um numero ')))
-n = str(n)
-if len(n)<5:
-    if len(n) == 1:
-        print('sim')
-    elif len(n) <= 4 and n[0] == n[-1] and n[1] == n[-2]:
-        print('sim')
-    else:
-        print('não')
+if n > 0:
+    n = str(n)
+    if len(n) < 5:
+        if len(n) == 1:
+            print('sim')
+        elif len(n) <= 4 and n[0] == n[-1] and n[1] == n[-2]:
+            print('sim')
+        else:
+            print('não')
+
 
 
 
