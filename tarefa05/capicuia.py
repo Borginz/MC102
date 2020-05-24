c3321a2c0c25e6733486224f1152f65b249f6517
unknown <l177829@unicamp.com>
2020-05-24 17:23:37 -0300

    4

--- tarefa05/capicuia.py
+++ tarefa05/capicuia.py
@@ -1,13 +1,13 @@
-n = int((input('digite um numero ')))
-if n > 0:
-    n = str(n)
-    if len(n) < 5:
-        if len(n) == 1:
-            print('sim')
-        elif len(n) <= 4 and n[0] == n[-1] and n[1] == n[-2]:
-            print('sim')
-        else:
-            print('não')
+n = input()
+nint = int(n)
+if nint <100 and n[0] == n[-1]:
+    print('sim')
+elif nint>=100 and nint<1000 and n[0] == n[-1]:
+    print('sim')
+elif nint>= 100 and nint<10000 and n[0] == n[-1] and n[1] ==n[-2]:
+    print('sim')
+else:
+    print('não')
 
 
 
