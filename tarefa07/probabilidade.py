3d6f6410a78d4f77ba24687eb236100ac3a81a98
unknown <l177829@unicamp.com>
2020-05-27 17:13:24 -0300

    arrumei

--- tarefa07/probabilidade.py
+++ tarefa07/probabilidade.py
@@ -1,40 +1,63 @@
-"""
-Tendo uma lista ( L ), faça a contagem das frequencias que nela aparece, elimine a repetição da lista original e ordene numa segunda lista
+def maior_L(L):
+    n = 0
+    for i in L:
+        i = int(i)
+        if i > n:
+            n = i
+    return n
+def inteirar(L):
+    for i in L:
+        i = int(i)
+    return L
+def contar_frequencia(L,n):
+    F = [0] * (n + 1)
+    for i in L:
+        i = int(i)
+        F[i] += 1
+    return F
+def tirar_zeros(F):
+    F2 = []
+    for i in F:
+        if i != 0:
+            F2.append(i)
+    return F2
+def tirar_repeticao(L):
+    L2 = []
+    for i in L:
+        if i not in L2:
+            L2.append(i)
+    return L2
+def ordenar_crescente(L2):
+    for i in range(len(L2) - 1):
+        for i in range(len(L2) - 1):
+            if L2[i] > L2[i + 1]:
+                aux = L2[i]
+                L2[i] = L2[i + 1]
+                L2[i + 1] = aux
+def ordenar_ocorrencias(L2,F2):
+    for j in range(len(L2)-1):
+        for w in range(len(F2)-1):
+            for i in range(len(F2)-1):
+                if F2[i] > F2[i+1]:
+                    aux = F2[i]
+                    aux2 = L2[i]
+                    F2[i] = F2[i+1]
+                    F2[i+1]= aux
+                    L2[i] = L2[i+1]
+                    L2[i+1] = aux2
+def devolver_L(L2):
+    for i in L2:
+        print(i, end=' ')
 
-"""
-L = input().split()
-L2= []
-n = 0
 
-for i in L:
-    i=int(i)
-    if i > n:
-        n=i
-F = [0]*(len(L)+1)
-for i in L:
-    i = int(i)
-    F[i] += 1
-F2 = []
-for i in F:
-    if i != 0:
-        F2.append(i)
-for i in L:
-    if i not in L2:
-        L2.append(i)
-for i in range(len(L2)-1):
-    for i in range(len(L2)-1):
-        if L2[i]> L2[i+1]:
-            aux = L2[i]
-            L2[i]= L2[i+1]
-            L2[i+1]= aux
-for i in range(len(F2)-1):
-    for i in range(len(F2)-1):
-        if F2[i]>F2[i+1]:
-            aux = F2[i]
-            F2[i] = F2[i + 1]
-            F2[i + 1] = aux
-            aux2 = L2[i]
-            L2[i] = L2[i + 1]
-            L2[i + 1] = aux
-for i in L2:
-    print(i, end=' ')
+def main():
+    L = input().split()
+    L = inteirar(L)
+    n = maior_L(L)
+    F = contar_frequencia(L,n)
+    F2 = tirar_zeros(F)
+    L2 = tirar_repeticao(L)
+    ordenar_crescente(L2)
+    ordenar_ocorrencias(L2,F2)
+    devolver_L(L2)
+main()
