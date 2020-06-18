7692d8157283fe7e5ce740ea1ade13468d713b92
unknown <l177829@unicamp.com>
2020-06-18 14:23:35 -0300

    arrumei os erros

--- tarefa07/probabilidade.py
+++ tarefa07/probabilidade.py
@@ -1,63 +1,75 @@
-def maior_L(L):
-    n = 0
-    for i in L:
+def maior_da_lista(lista):
+    '''função para achar o maior numero da lista'''
+    maior = 0
+    for i in lista:
         i = int(i)
-        if i > n:
-            n = i
-    return n
-def inteirar(L):
-    for i in L:
+        if i > maior:
+            maior = i
+    return maior
+def inteirar(lista):
+    '''Função para transformar as 'str''em int'''
+    for i in lista:
         i = int(i)
-    return L
-def contar_frequencia(L,n):
-    F = [0] * (n + 1)
-    for i in L:
+    return lista
+def contar_frequencia(lista,maior):
+    '''função para contar a frequencia e assim a probabilidade de cada numero'''
+    frequencia = [0] * (maior + 1)
+    for i in lista:
         i = int(i)
-        F[i] += 1
-    return F
-def tirar_zeros(F):
-    F2 = []
-    for i in F:
+        frequencia[i]+= 1
+    return frequencia
+def tirar_zeros(lista):
+    '''funçao para tirar os zeros da lista de frequencia deixando em ordem'''
+    frequencia_sem_zeros = []
+    for i in lista:
         if i != 0:
-            F2.append(i)
-    return F2
-def tirar_repeticao(L):
-    L2 = []
-    for i in L:
-        if i not in L2:
-            L2.append(i)
-    return L2
-def ordenar_crescente(L2):
-    for i in range(len(L2) - 1):
-        for i in range(len(L2) - 1):
-            if L2[i] > L2[i + 1]:
-                aux = L2[i]
-                L2[i] = L2[i + 1]
-                L2[i + 1] = aux
-def ordenar_ocorrencias(L2,F2):
-    for j in range(len(L2)-1):
-        for w in range(len(F2)-1):
-            for i in range(len(F2)-1):
-                if F2[i] > F2[i+1]:
-                    aux = F2[i]
-                    aux2 = L2[i]
-                    F2[i] = F2[i+1]
-                    F2[i+1]= aux
-                    L2[i] = L2[i+1]
-                    L2[i+1] = aux2
-def devolver_L(L2):
-    for i in L2:
+            frequencia_sem_zeros.append(i)
+    return frequencia_sem_zeros
+def tirar_repeticao(lista):
+    ''' função para tirar numeros repetidos para poder fazer a contagem da saída'''
+    lista_sem_repeticao = []
+    for i in lista:
+        if i not in lista_sem_repeticao:
+            lista_sem_repeticao.append(i)
+    return lista_sem_repeticao
+
+def ordenar_crescente(lista):
+    '''Função para ordenar uma lista em ordem crescente'''
+    for i in range(len(lista)-1):
+        for i in range(len(lista)-1):
+            if lista[i] > lista[i + 1]:
+                aux = lista[i]
+                lista[i] = lista[i + 1]
+                lista[i + 1] = aux
+    return lista
+
+def ordenar_probabilidade(lista,lista2):
+    '''Função para ordenar uma lista em função da outra'''
+    for i in range(len(lista)-1):
+        for i in range(len(lista)-1):
+            if lista2[i] > lista2[i + 1]:
+                aux = lista2[i]
+                aux2 = lista[i]
+                lista2[i] = lista2[i + 1]
+                lista2[i + 1] = aux
+                lista[i] = lista[i + 1]
+                lista[i + 1] = aux2
+
+
+def devolver_lista(lista):
+     '''Função para devolver a saída da lista em ordem de probabilidade'''
+    for i in lista:
         print(i, end=' ')
 
 
 def main():
-    L = input().split()
-    L = inteirar(L)
-    n = maior_L(L)
-    F = contar_frequencia(L,n)
-    F2 = tirar_zeros(F)
-    L2 = tirar_repeticao(L)
-    ordenar_crescente(L2)
-    ordenar_ocorrencias(L2,F2)
-    devolver_L(L2)
+    lista_entrada = input().split()
+    lista_entrada = inteirar(lista_entrada)
+    maior = maior_da_lista(lista_entrada)
+    frequencia = contar_frequencia(lista_entrada,maior)
+    frequencia_sem_zeros = tirar_zeros(frequencia)
+    lista_sem_repeticao = tirar_repeticao(lista_entrada)
+    lista_ordem = ordenar_crescente(lista_sem_repeticao)
+    ordenar_probabilidade(lista_ordem,frequencia_sem_zeros)
+    devolver_lista(lista_sem_repeticao)
 main()
