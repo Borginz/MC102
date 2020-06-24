8023f6df7e1dee40fabb29d87ae705a14719606c
unknown <l177829@unicamp.com>
2020-06-24 18:57:07 -0300

    arrumei função ordenar

--- tarefa07/probabilidade.py
+++ tarefa07/probabilidade.py
@@ -19,13 +19,29 @@ def ordenar_listas(lista,lista2):
     """Função para organizar a freequencia com os eventos unicos"""
     for i in range(len(lista)-1):
         for i in range(len(lista)-1):
-            if lista2[i] > lista2[i+1]:
+            if lista2[i]==lista2[i+1] and lista[i]>lista[i+1]:
                 aux = lista2[i]
                 aux2 = lista[i]
                 lista2[i] = lista2[i+1]
-                lista2[i+1] = aux
                 lista[i] = lista[i+1]
+                lista2[i+1] = aux
                 lista[i+1] = aux2
+            elif lista2[i]>lista2[i+1]:
+                aux = lista2[i]
+                aux2 = lista[i]
+                lista2[i] = lista2[i + 1]
+                lista[i] = lista[i + 1]
+                lista2[i + 1] = aux
+                lista[i + 1] = aux2
+
+
+
+
+
+
+
+
+
 def devolver_lista(lista):
     """Função para devolver separado pos espaços a lista"""
     for i in lista:
@@ -35,6 +51,8 @@ def main():
     lista_entrada = [int(x) for x in input().split()]
     lista_sem_repeticao = tirar_repeticao(lista_entrada)
     frequencia = contar_frequencia(lista_entrada, lista_sem_repeticao)
+    print(lista_sem_repeticao)
+    print (frequencia)
     ordenar_listas(lista_sem_repeticao, frequencia)
     devolver_lista(lista_sem_repeticao)
 main()
