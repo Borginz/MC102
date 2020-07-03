2c14785c3703e2bbca04cbb24406eb46afa995cf
unknown <l177829@unicamp.com>
2020-07-03 15:55:37 -0300

    Função carregar imagem deco

--- tarefa10/modulo.py
+++ tarefa10/modulo.py
@@ -1,9 +1,7 @@
 def codificar(largura, altura, imagem):
-    with open("imagem.txt") as arquivo:
-        imagem = []
-        for linha in arquivo:
-            linhas = linha.strip()
-            imagem.append(linhas)
+
+
+
 
 
     return codificacao
@@ -14,16 +12,29 @@ def decodificar(largura, altura, codificacao):
 
 
 def carregar_imagem_codificada(nome_do_arquivo):
+
+
+
     return largura, altura, codificacao
 
 
 def carregar_imagem_decodificada(nome_do_arquivo):
+    with open(nome_do_arquivo) as arquivo:
+        imagem = []
+        for linha in arquivo:
+            linhas = linha.strip()
+            imagem.append(linhas)
+            altura = len(imagem)
+            largura = len(imagem[0])
     return largura, altura, imagem
 
 
 def escrever_imagem_codificada(largura, altura, codificacao, nome_do_arquivo):
+
+
     pass
 
 
 def escrever_imagem_decodificada(largura, altura, imagem, nome_do_arquivo):
-    pass
+
+
