465a88eb04d29d0de66a80be8be923fe49125c38
unknown <l177829@unicamp.com>
2020-07-15 00:34:01 -0300

    4

--- tarefa10/sdds.py
+++ tarefa10/sdds.py
@@ -1,11 +1,22 @@
-imagem = [[] for _ in range(altura)]
+def decodificar(largura, altura, codificacao):
+    imagem = [[] for _ in range(altura)]
     imagem_codificacao = []
-    for i in range(0,codificacao,2):
-        imagem_codificacao.append(codificacao[i])
+    for i in range(0, len(codificacao), 2):
+        for _ in range(codificacao[i]):
+            imagem_codificacao.append(codificacao[i + 1])
     multiplicador = 0
-    for k in range(0,altura,2):
+    for k in range(0, altura, 2):
         for j in range(largura):
-            imagem[k].append((imagem_codificacao[multiplicador*largura + j][0]))
-            imagem[k+1].append((imagem_codificacao[multiplicador*largura + j][1]))
-        multiplicador+=1
+            imagem[k].append(str(imagem_codificacao[(multiplicador * largura)+j][0]))
+            imagem[k+1].append(str(imagem_codificacao[(multiplicador * largura)+j][1]))
+        multiplicador += 1
     return imagem
+
+
+def main():
+    largura = 8
+    altura = 6
+    codificacao = [4,'01',4,'00',16,'11']
+    print(decodificar(largura,altura,codificacao))
+
+main()
