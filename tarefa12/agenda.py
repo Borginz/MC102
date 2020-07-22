95644c6547b04f82c2121afbe80ee3a54ccbc5df
unknown <l177829@unicamp.com>
2020-07-22 17:31:08 -0300

    2

--- tarefa12/agenda.py
+++ tarefa12/agenda.py
@@ -1,9 +1,21 @@
  import argparse
+def obter_argumentos():
+    parser = argparse.ArgumentParser()
+    '''Adicionar os argumentos que serão usados '''
+    parser.add_argument('a', help = "argumento para executar o arquivo",default='agenda.csv')
+    parser.add_argument('operacao', help = "operacoes que queremos realizar na agenda")
+    parser.add_argument('--evento', help = 'mostra qual o evento',type=int)
+    parser.add_argument('--nome', help = 'mostra o nome do evento')
+    parser.add_argument('--descricao', help='descricao do evento')
+    parser.add_argument('--data', help='data do evento')
+    parser.add_argument('--hora', help= 'hora do evento')
+
+
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-a", help = "argumento para executar aplicativo")
-    parser.add_argument("operacoes",help = "operacoes em agenda")
-    args = parser.parse_args()
+    argumentos = obter_argumentos()
+
+
+
 
