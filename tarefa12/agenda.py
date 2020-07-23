56d1f6bab8c9c15d56e7a98f587948db427e8b85
unknown <l177829@unicamp.com>
2020-07-23 01:34:30 -0300

    a

--- tarefa12/agenda.py
+++ tarefa12/agenda.py
@@ -2,7 +2,7 @@
 def obter_argumentos():
     parser = argparse.ArgumentParser()
     '''Adicionar os argumentos que serão usados '''
-    parser.add_argument('-a',help = "argumento para executar o arquivo", required=True)
+    parser.add_argument('-a','--agenda'help = "argumento para executar o arquivo", required=True)
     parser.add_argument('operacao', help = "operacoes que queremos realizar na agenda")
     parser.add_argument('--evento', help = 'mostra qual o evento',type=int)
     parser.add_argument('--nome', help = 'mostra o nome do evento')
@@ -13,37 +13,71 @@ def obter_argumentos():
     args = parser.parse_args()
     return args
 
-def executar_acao(args):
-    agenda = args.a
+'''def executar_acao(args):
+    agenda = args.agenda
     operacao = args.operacao
     evento = args.evento
     nome = args.nome
     descricao = args.descricao
     data = args.data
     hora = args.hora
+    anotacoes_agenda = dict()'''
 
-    if operacao == "inicializar":
-        iniciar_agenda(agenda)
 
-    if operacao == "criar":
-        pass
+def inicializar_agenda(nome_arquivo):
+    with open(nome_arquivo,'w') as arquivo:
+        arquivo.write(dict())
+    print('Uma agenda vazia "agenda.csv" foi criada!')
+    return
 
-    if operacao == "alterar":
-        pass
+def ler_arquivo(agenda_csv):
+    with open(agenda_csv) as arquivo:
+        agenda = arquivo.readlines()
+        agenda = eval(agenda)
+    return agenda
 
-    if operacao == "listar":
-        pass
+def alterar(evento,nome,descricao,data,hora,agenda):
 
-    if operacao == "remover":
-        pass
 
-def iniciar_agenda(agenda):
-    
+
+
+
+def criar_agenda(nome,descricao,data,hora,agenda):
+
+
+
+
+
 
 
 def main():
-    argumentos = obter_argumentos()
-    executar_acao(argumentos)
+     parser = argparse.ArgumentParser()
+     '''Adicionar os argumentos que serão usados '''
+     parser.add_argument('-a', '--agenda', help = "argumento para executar o arquivo", required = True)
+     parser.add_argument('operacao', help="operacoes que queremos realizar na agenda")
+     parser.add_argument('--evento', help='mostra qual o evento', type=int)
+     parser.add_argument('--nome', help='mostra o nome do evento')
+     parser.add_argument('--descricao', help='descricao do evento')
+     parser.add_argument('--data', help='data do evento')
+     parser.add_argument('--hora', help='hora do evento')
+     args = parser.parse_args()
+     if args.operacao == 'inicializar':
+         inicializar_agenda(args.agenda)
+     else:
+         agenda = ler_arquivo(args.agenda)
+         if args.operacao == 'criar':
+             criar_agenda(agenda)
+         elif args.operacao == 'alterar':
+              alterar_agenda(agenda)
+         elif args.operacao == 'remover':
+              remover_agenda(agenda)
+         elif args.operacao == 'listar':
+             listar_agenda(agenda)
+
+
+
+
+
 
 
 
