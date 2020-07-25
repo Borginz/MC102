8308e3efd2507aab06cd15592949b7b3c2a37edd
unknown <l177829@unicamp.com>
2020-07-25 03:42:43 -0300

    dormir

---
+++ tarefa14/collatz.py
@@ -0,0 +1,17 @@
+
+def collatz(n,contador):
+    if n == 1:
+        return 0
+    elif n % 2 == 0:
+        return collatz(n/2,contador)
+    else:
+        return collatz((3*n+1)/2,contador)
+
+
+
+def main():
+    numero = int(input())
+    contador = 0
+    collatz(numero,contador)
+    print(contador)
+main()
