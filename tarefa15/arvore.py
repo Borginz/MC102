b9d815d8ed5508fffc8fc14a1117e3048022b7d1
unknown <l177829@unicamp.com>
2020-07-25 22:18:40 -0300

    0

---
+++ tarefa15/arvore.py
@@ -0,0 +1,13 @@
+from modulo import*
+
+
+def mostrar_arvore():
+
+
+def main():
+    url_inicial = input()
+    lista_printados = []
+    espaços = 0
+    url_base = url_inicial
+    url_atual =url_inicial
+    mostrar_arvore(url_inicial,url_base,url_atual,lista_printados,espaços)
