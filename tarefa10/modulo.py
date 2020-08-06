d504f8675a66d4f7e82265ff0f59453720e66882
unknown <l177829@unicamp.com>
2020-08-06 18:43:11 -0300

    3

--- tarefa10/modulo.py
+++ tarefa10/modulo.py
@@ -1,13 +1,24 @@
 def codificar(largura, altura, imagem):
-    padraoi == imagem[0][0]
-    padraoj == imagem[1][0]
     codificacao = []
-    for idx in range(0,altura,2):
-        for codificacoes in range(largura):
-            decodificacao.append(imagem[idx][codificacoes]+imagem[idx][codificacoes])
-    for idx i
-
-
+    imagem_linear = []
+    for idx_alt in range(0,altura,2):
+        for idx_larg in range(largura):
+            imagem_linear.append(imagem[idx_alt][idx_larg]+imagem[idx_alt+1][idx_larg])
+    padrao = imagem_linear[0]
+    contador = 0
+    for idx_linear in range(len(imagem_linear)):
+        if imagem_linear[idx_linear] == padrao:
+            if idx_linear == len(imagem_linear)-1:
+                contador += 1
+                codificacao.append(str(contador))
+                codificacao.append(padrao)
+            else:
+                contador += 1
+        else:
+            codificacao.append(str(contador))
+            codificacao.append(padrao)
+            padrao = imagem_linear[idx_linear+1]
+            contador = 1
 
 
     return codificacao
--- tarefa10/testesss.py
+++ tarefa10/testesss.py
@@ -1,25 +1,41 @@
 def codificar(largura, altura, imagem):
-    '''padraoi == imagem[]
     codificacao = []
-    for idx in range(0,altura,2):
-        for codificacoes in range(largura):
-            decodificacao.append(imagem[idx][codificacoes]+imagem[idx][codificacoes])
-    for idx i'''
+    imagem_linear = []
+    for idx_alt in range(0,altura,2):
+        for idx_larg in range(largura):
+            imagem_linear.append(imagem[idx_alt][idx_larg]+imagem[idx_alt+1][idx_larg])
+    padrao = imagem_linear[0]
+    contador = 0
+    for idx_linear in range(len(imagem_linear)):
+        if imagem_linear[idx_linear] == padrao:
+            if idx_linear == len(imagem_linear)-1:
+                contador += 1
+                codificacao.append(str(contador))
+                codificacao.append(padrao)
+            else:
+                contador += 1
+        else:
+            codificacao.append(str(contador))
+            codificacao.append(padrao)
+            padrao = imagem_linear[idx_linear+1]
+            contador = 1
 
 
 
 
-    '''return codificacao'''
+
+
+
+
+
+
+
+    return codificacao,imagem_linear
 def main():
     largura = 8
     altura = 6
-    imagem = [['0','0','0','0','0','0','0','0'],
-              ['1','1','1','1','0','0','0','0'],
-              ['1','1','1','1','1','1','1','1'],
-              ['1','1','1','1','1','1','1','1'],
-              ['1','1','1','1','1','1','1','1'],
-              ['1','1','1','1','1','1','1','1']]
-    codificacao = codificar(largura,altura,imagem)
+    imagem = [['0','0','0','0','0','0','0','0'],['1','1','1','1','0','0','0','0'],['1','1','1','1','1','1','1','1'],['1','1','1','1','1','1','1','1'],['1','1','1','1','1','1','1','1'],['1','1','1','1','1','1','1','1']]
+    codificacao,imagem = codificar(largura,altura,imagem)
     print(imagem)
     print(codificacao)
 main()
