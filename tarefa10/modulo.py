5f793ed7efa3acf8056a56a1167606a840f7648c
unknown <l177829@unicamp.com>
2020-08-06 22:32:33 -0300

    4

--- tarefa10/modulo.py
+++ tarefa10/modulo.py
@@ -25,55 +25,45 @@ def codificar(largura, altura, imagem):
 
 
 def decodificar(largura, altura, codificacao):
-    imagem_decodificada = []
     imagem = [[] for _ in range(altura)]
-
-
-
-
+    imagem_decodificada = []
+    for idx_freq in range(0,len(codificacao),2):
+        for freq in range(int(codificacao[idx_freq])):
+            imagem_decodificada.append(codificacao[idx_freq+1])
+    contador = 0
+    for idx_linha in range(0,altura,2):
+        for idx_coluna in range(largura):
+            imagem[idx_linha].append(str(imagem_decodificada[(contador*largura)+idx_coluna][0]))
+            imagem[idx_linha+1].append(str(imagem_decodificada[(contador*largura)+idx_coluna][1]))
+        contador += 1
     return imagem
-
 
 def carregar_imagem_codificada(nome_do_arquivo):
     with open(nome_do_arquivo) as arquivo:
-        P1C = arquivo.readline().strip()
+        arquivo.readline()
         larg_alt = arquivo.readline().strip()
-        codificacao = arquivo.readline().strip().split()
-        altura,largura = larg_alt.split()
-        altura = int(altura)
+        largura,altura = larg_alt.split()
         largura = int(largura)
-
+        altura = int(altura)
+        codificacao = arquivo.readline().strip().split()
 
     return largura, altura, codificacao
 
 
 def carregar_imagem_decodificada(nome_do_arquivo):
+    imagem = []
     with open(nome_do_arquivo) as arquivo:
-        P1C = arquivo.readline().strip()
-        larg_alt = arquivo.readline().strip()
-        altura, largura = larg_alt.split()
-        altura = int(altura)
+        arquivo.readline()
+        largura,altura = arquivo.readline().strip().split()
         largura = int(largura)
-        imagem = []
+        altura = int(altura)
         cont_linha = 0
-        idx_coluna = 0
-        while cont_linha < altura:
+        while cont_linha != altura:
             linha = arquivo.readline().strip()
             imagem.append([])
-            while idx_coluna < largura:
-                imagem[cont_linha].append(linha[idx_coluna])
-                idx_coluna+=1
-        cont_linha+=1
-
-
-
-
-#for elemento in linha:
-#imagem[cont_linha].append(elemento)
-#cont_linha+=1
-
-
-
+            for elemento in linha:
+                imagem[cont_linha].append(elemento)
+            cont_linha += 1
 
 
     return largura, altura, imagem
@@ -82,36 +72,19 @@ def carregar_imagem_decodificada(nome_do_arquivo):
 def escrever_imagem_codificada(largura, altura, codificacao, nome_do_arquivo):
     with open(nome_do_arquivo, 'w') as arquivo:
         arquivo.write('P1C\n')
-        arquivo.write(f'{largura} {altura}\n')
-        arquivo.write(f'{codificacao}\n')
+        arquivo.write(largura+' '+altura+'\n')
+        for elementos in codificacao:
+            print(elementos,end=' ',file=arquivo)
 
 
 
 def escrever_imagem_decodificada(largura, altura, imagem, nome_do_arquivo):
     with open(nome_do_arquivo, 'w') as arquivo:
         arquivo.write(f'P1C\n')
-        arquivo.write(largura,altura,'\n')
+        arquivo.write(largura+' '+altura+'\n')
         for idx_linha in range(altura):
-            for elemento in imagem[idx_linha]:
-                if not idx_linha == altura-1:
-                    arquivo.write(f'{elemento}')
+            for idx in range(largura):
+                if not idx == altura-1:
+                    arquivo.write(imagem[idx_linha][idx])
                 else:
-                    arquivo.write(f'{elemento}\n')
-
-
-
-
-
-
-def main():
-
-    m, n, codificacao = carregar_imagem_codificada(input())
-    imagem = decodificar(m, n, codificacao)
-    nova_imagem = destacar_bordas(m, n, imagem)
-
-    codificacao = codificar(m, n, nova_imagem)
-    escrever_imagem_codificada(m, n, codificacao, input())
-
-
-if __name__ == '__main__':
-    main()
+                    arquivo.write(imagem[idx_linha][idx]+'\n')
---
+++ tarefa10/testes2.py
@@ -0,0 +1,5 @@
+larg_alt = "8 6"
+print(larg_alt)
+largura,altura = larg_alt.split()
+print(largura,altura)
+print(largura+' '+altura)
