f86a7a452b1dc72a84894a418c9dec765fc74656
unknown <l177829@unicamp.com>
2020-08-07 19:56:17 -0300

    2

--- tarefa10/bordas.py
+++ tarefa10/bordas.py
@@ -1,16 +1,17 @@
 from modulo import *
 
 def destacar_bordas(largura,altura,imagem):
-    nova_imagem = imagem[:]
+    nova_imagem = []
+    for idx_alt in range(altura):
+        for idx_larg in range(largura):
+            nova_imagem.append(imagem[idx_alt][idx_larg])
     for i in range(1,altura-1):
         for j in range(1,largura-1):
             if nova_imagem[i][j] == '1':
-                if imagem[i-1][j] == '0' or imagem[i-1][j+1] == '0' or imagem[i-1][j-1] == '0' or imagem[i][j+1] == '0' or imagem[i][j-1] == '0' or imagem[i][j] == '0' or imagem[i+1][j+1] == '0' or imagem[i+1][j] == '0':
+                if imagem[i][j-1] == '0' or imagem[i][j+1] == '0' or imagem[i+1][j] == '0' or imagem[i+1][j-1] == '0' or imagem[i+1][j+1] or imagem[i-1][j] == '0' or imagem[i-1][j+1] == '0' or imagem[i-1][j-1] == '0':
                     imagem[i][j] = '1'
                 else:
                     imagem[i][j] = '0'
-            else:
-                imagem[i][j] = '0'
 
 
     return imagem
