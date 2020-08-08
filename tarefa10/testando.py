ac9ef2517f9cdeb9123a49a6235c1e6c4d45567f
unknown <l177829@unicamp.com>
2020-08-08 20:12:50 -0300

    aa

--- tarefa10/testando.py
+++ tarefa10/testando.py
@@ -1,5 +1,13 @@
-matriz= [[1],[2],[3]]
-b = matriz[1:-1]
-
-print(matriz)
-print(b)
+def destacar_bordas(largura,altura,imagem):
+    nova_imagem = imagem[:]
+    for i in range(1,altura-1):
+        for j in range(1,largura-1):
+            if imagem[i][j] == '1':
+                if imagem[i][j-1] == '0' or imagem[i][j+1] == '0' or imagem[i+1][j] == '0' or imagem[i+1][j-1] == '0' \
+                        or imagem[i+1][j+1] or imagem[i-1][j] == '0' or imagem[i-1][j+1] == '0' or imagem[i-1][j-1] == '0':
+                    nova_imagem[i][j] = '1'
+                else:
+                    nova_imagem[i][j] = '0'
+    print(nova_imagem)
+    return nova_imagem
+def
